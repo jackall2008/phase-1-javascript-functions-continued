8e7fce049473a5c20df45168bd3846c377acc6bf
Julian Patterson <julian.patterson@gmail.com>
2023-01-10 03:44:43 -0600

    This file passes all 9 tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    const fullFunStatement = `This Saturday, I want to ${activity}!`;
+    return fullFunStatement;
+};
+
+const mondayWork = function (activity = `go to the office`) {
+    const fullWorkStatement = `This Monday, I will ${activity}.`;
+
+    return fullWorkStatement;
+}
+
+function wrapAdjective(string = "*") {
+    return function(adjective = "special") {
+        const newString = `You are ${string}${adjective}${string}!`;
+        return newString;
+    }
+}
